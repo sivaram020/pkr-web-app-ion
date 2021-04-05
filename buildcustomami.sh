b50fbfa47889e530296412787fcdadecd822ce9a
sivaram <sivaramd020@gmail.com>
2021-04-05 12:05:04 +0530

    update in buildcustomami.sh
    update in buildcustomami.sh

--- buildcustomami.sh
+++ buildcustomami.sh
@@ -2,7 +2,7 @@
 sudo su
 cd
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install libtomcat9-java -y
+sudo apt-get install libtomcat9-java -y
 sudo apt-get update -y
 sudo apt-get install tomcat9-admin tomcat9-common -y
 sudo apt-get install tomcat9 -y
